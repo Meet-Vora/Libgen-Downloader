5f0f2866978c66b7147646b2644ac3d84a507760
Meet Vora <meetrajivvora@gmail.com>
2021-12-23 18:51:11 -0800

    adding beautiful soup web scraping functionality

--- libgen.py
+++ libgen.py
@@ -1,6 +1,12 @@
 import pyinputplus as pyip
+from argparse import ArgumentParser
 from search import SearchRequest
 from pprint import pprint
+
+test_is_fiction = True
+test_title = 'sword of kaigen'
+test_author = 'wang'
+test_year = None
 
 def menu():
     is_fiction = True if pyip.inputMenu(prompt="\nWhat category does your text fall into?\n", choices=[
@@ -12,19 +18,26 @@ def menu():
         author = pyip.inputStr(prompt="Author: ", blank=True)
         title = pyip.inputStr(prompt="Title: ", blank=True)
 
-        if not author and not title:
-            print('\nYou cannot leave both author and title fields empty. Please enter in at least one')
-            continue
+        if author or title:
+            year = pyip.inputNum(prompt="Year: ", blank=True) # can parse the input for 0 <= year <= current year
+            break
 
-        year = pyip.inputNum(prompt="Year: ", blank=True) # can parse the input for 0 <= year <= current year
-        break
+        print('\nYou cannot leave both author and title fields empty. Please enter in at least one')
 
-    return {'is_fiction': is_fiction, 'author': author, 'title': title, 'year': year}
+    return {'is_fiction': is_fiction, 'author': author.lower(), 'title': title.lower(), 'year': year}
 
     # extension = pyip.
 
 
 if __name__ == "__main__":
-    inputs = menu()
-    searchrequest = SearchRequest(is_fiction=inputs['is_fiction'], title=inputs['title'], author=inputs['author'], year=inputs['year'])
+    parser = ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true')
+    args = parser.parse_args()
+
+    if args.debug:
+        searchrequest = SearchRequest(is_fiction=test_is_fiction, title=test_title, author=test_author, year=test_year)
+    else:
+        inputs = menu()
+        searchrequest = SearchRequest(is_fiction=inputs['is_fiction'], title=inputs['title'], author=inputs['author'], year=inputs['year'])
+    searchrequest.search()
     # pprint(search_title("the sword of kaigen"))
--- search.py
+++ search.py
@@ -3,28 +3,43 @@ from bs4 import BeautifulSoup
 import requests
 libgen = LibgenSearch()
 
-BASE_URL = 'libgen.is/fiction?q='
 
 class SearchRequest():
     def __init__(self, is_fiction, author, title, year):
+        self.base_fiction_url = 'https://libgen.is/fiction/?q='
         self.is_fiction = is_fiction
         self.author = author
         self.title = title
+        self.year = year
         
-    def search_title(title, filters):
+    def _search_title(self):
         """
         Search for nonfiction novel using the given title and filters.
         """
-        return libgen.search_title(title)
+        return libgen.search_title(self.title)
 
-    def search_author(author, filters):
+    def _search_author(self):
         """
         Search for nonfiction novel using the given author and filters.
         """
-        return libgen.search_author(author)
+        return libgen.search_author(self.author)
 
-    def search_query(author, title, filters):
+    def _search_fictional(self):
         """
         Search for fiction novel using the given author, title, and filters.
         """
-        query = author + title
+        query = (self.author + ' ' + self.title).replace(' ', '+')
+        url = self.base_fiction_url + query
+        print(url)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # assuming that the content was found. Need to still check if the request found actual values
+        table_rows = soup.find('table', class_='catalog').tbody.find_all('tr')
+        print(len(table_rows))
+    
+    def search(self):
+        if self.is_fiction:
+            return self._search_fictional()
+        if not self.title:
+            return self._search_author()
+        return self._search_title()
